150c392b2ed18d6aef73dd0eb308d7836a8e7adf
yangkaixuan <439721838@qq.com>
2018-08-04 22:48:38 +0800

    添加了

--- game/js/main.js
+++ game/js/main.js
@@ -9,9 +9,10 @@ import BackGround from './runtime/background'
 import GameInfo from './runtime/gameinfo'
 import Music from './runtime/music'
 import DataBus from './databus'
+import HomePage from './runtime/homepage'
 
-const screenWidth = window.innerWidth
-const screenHeight = window.innerHeight
+
+
 
 
 
@@ -19,8 +20,8 @@ const screenHeight = window.innerHeight
 let hero = new Image()
 hero.src = 'images/hero.png'
 
-let a = new Image()
-a.src = 'images/a.png';
+const screenWidth = window.innerWidth
+const screenHeight = window.innerHeight
 
 
 let ctx = canvas.getContext('2d')
@@ -30,21 +31,33 @@ let databus = new DataBus()
 
 /**
  * 游戏主函数
+ * author:yangkaixuan
+ * date:2018-08-04
  */
+
+
 export default class Main {
   constructor() {
-    var width = canvas.width;
-    var height = canvas.height;
+
+    //实例化分数系统对象
+    this.gameinfo = new GameInfo()
+    //实例化分数音效对象
+    this.music = new Music()
+    //实例化home页面对象
+    this.home = new HomePage()
+
 
     //解决在真机上画图渲染的锯齿问题 具体逻辑查看微信
     if(window.devicePixelRatio){      
+      var width = canvas.width;
+      var height = canvas.height;
       canvas.style.width = width + "px"
       canvas.style.height = height + "px"
       canvas.height = height * window.devicePixelRatio
       canvas.width = width * window.devicePixelRatio
       ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
     }
-    this.first = true;
+
 
     // 维护当前requestAnimationFrame的id
     this.aniId = 0
@@ -65,8 +78,7 @@ export default class Main {
           
       }
      
-        
-  
+
 
       this.player.tem_ = e.touches[0].clientX - this.player.x - 40;
       this.player.tem_y = e.touches[0].clientY - this.player.y;
@@ -101,8 +113,7 @@ export default class Main {
     )
 
     this.player = new Player(ctx)
-    this.gameinfo = new GameInfo()
-    this.music = new Music()
+
 
     this.bindLoop = this.loop.bind(this)
     this.hasEventBind = false
@@ -373,112 +384,7 @@ export default class Main {
 
   mainrender() {    
     this.bg.render(ctx)
-    //背景颜色为灰色
-    ctx.fillStyle = "#84858c";
-    ctx.globalAlpha = 0.6
-    ctx.fillRect(0,0,screenWidth,screenHeight);      
-    ctx.save();
-    ctx.globalAlpha = 1;
-    
-    ctx.drawImage(
-      a,
-      screenWidth/2 - 100,
-      120,
-      200,
-      48
-    )
-
- 
-    ctx.save();
-    // //先画一长方形，背景色为白    
-    ctx.fillStyle = "#fff";
-    ctx.fillRect(screenWidth / 2 - 65, screenHeight/2  + 40, 130, 50);   
-    ctx.save();
-
-    //画一个半圆（右）
-    ctx.beginPath();
-    ctx.fillStyle = "#fff";
-    ctx.arc(screenWidth / 2 + 64, screenHeight / 2 + 65, 25, 0 - 0.5 * Math.PI, 0.5 * Math.PI);    
-    ctx.fill();
-    ctx.save();
-
-    //画一个半圆（左）
-    
-    ctx.beginPath();
-    ctx.fillStyle = "#fff";
-    ctx.arc(screenWidth / 2 - 64, screenHeight / 2 + 65, 25 , 0 + 0.5 * Math.PI, 1.5 * Math.PI);
-    ctx.fill();
-    
-    ctx.save();
-
-    //画一个三角形
-    ctx.beginPath();
-    var startx = screenWidth / 2 - 62;
-    var starty = screenHeight / 2 + 50;
-    ctx.moveTo(startx, starty);
-    ctx.lineTo(startx + 23, starty+15);
-    ctx.lineTo(startx, starty + 30);
-    ctx.lineTo(startx, starty);
-    ctx.fillStyle = "#588ddc";
-    ctx.fill(); 
-    ctx.save();
-
-    ctx.fillStyle = "#000";
-    ctx.font = "bold 23px Arial"
-    ctx.fillText(
-      "开始游戏",
-      screenWidth / 2 - 28,
-      screenHeight / 2 + 73
-    )
-
-    ctx.beginPath();
-    var width = canvas.width,
-      height = canvas.height;
-    if (window.devicePixelRatio) {
-      
-    }
-
-    // //画一条线
-    ctx.beginPath();
-    ctx.moveTo(0, screenHeight-150);
-    ctx.lineTo(screenWidth, screenHeight - 150);
-    ctx.strokeStyle = "#fff"
-    ctx.stroke();
-  
-
-
-    ctx.beginPath();
-    ctx.arc(screenWidth /2-100, screenHeight - 80, 40, 0, 2 * Math.PI);    
-    ctx.stroke();
-    ctx.font = "bold 23px Arial"
-    ctx.fillStyle = "#fff";
-    ctx.fillText(
-      "排行",
-      screenWidth / 2 - 123,
-      screenHeight - 74
-    )
-
-
-
-    ctx.beginPath();
-    ctx.arc(screenWidth / 2, screenHeight - 80, 40, 0, 2 * Math.PI);   
-    ctx.stroke();
-
-    ctx.font = "bold 23px Arial"
-    ctx.fillStyle = "#fff";
-    ctx.fillText(
-      "分享",
-      screenWidth / 2 - 22,
-      screenHeight - 74
-    )
-    ctx.beginPath();
-    ctx.arc(screenWidth / 2 + 100, screenHeight - 80, 40, 0, 2 * Math.PI);  
-    ctx.stroke();
-    ctx.fillText(
-      "设置",
-      screenWidth / 2 + 77,
-      screenHeight - 74
-    )
+    this.home.showpage(ctx)
 
 
   }
--- game/js/runtime/homepage.js
+++ game/js/runtime/homepage.js
@@ -1,47 +1,128 @@
-let instance
 
 /**
- * 统一的音效管理器
+ * 首页
+ * author:yangkaixuan
+ * date:2018-08-04
  */
-export default class homepage {
+
+const screenWidth = window.innerWidth
+const screenHeight = window.innerHeight
+
+//初始化
+let title = new Image()
+title.src = 'images/title.png';
+
+
+export default class HomePage {
   constructor() {
-    if ( instance )
-      return instance
-
-    instance = this
-
-    this.bgmAudio = new Audio()
-    this.bgmAudio.loop = true
-    this.bgmAudio.src  = 'audio/bgm.mp3'
-
-    this.shootAudio     = new Audio()
-    this.shootAudio.src = 'audio/bullet.mp3'
-
-    this.boomAudio     = new Audio()
-    this.boomAudio.src = 'audio/boom.mp3'
-
-    this.attackedAudio = new Audio()
-    this.attackedAudio.src = 'audio/attacked.mp3'
-
-    this.playBgm()
+ 
   }
+  showpage(ctx){
+    //背景颜色为灰色
+    ctx.fillStyle = "#84858c";
+    ctx.globalAlpha = 0.6
+    ctx.fillRect(0, 0, screenWidth, screenHeight);
+    ctx.save();
+    ctx.globalAlpha = 1;
 
-  playBgm() {
-    this.bgmAudio.play()
-  }
+    ctx.drawImage(
+      title,
+      screenWidth / 2 - 100,
+      120,
+      200,
+      48
+    )
 
-  playShoot() {
-    this.shootAudio.currentTime = 0
-    this.shootAudio.play()
-  }
 
-  playExplosion() {
-    this.boomAudio.currentTime = 0
-    this.boomAudio.play()
-  }
+    ctx.save();
+    // //先画一长方形，背景色为白    
+    ctx.fillStyle = "#fff";
+    ctx.fillRect(screenWidth / 2 - 65, screenHeight / 2 + 40, 130, 50);
+    ctx.save();
 
-  playattacked() {
-    this.attackedAudio.currentTime = 0
-    this.attackedAudio.play()
+    //画一个半圆（右）
+    ctx.beginPath();
+    ctx.fillStyle = "#fff";
+    ctx.arc(screenWidth / 2 + 64, screenHeight / 2 + 65, 25, 0 - 0.5 * Math.PI, 0.5 * Math.PI);
+    ctx.fill();
+    ctx.save();
+
+    //画一个半圆（左）
+
+    ctx.beginPath();
+    ctx.fillStyle = "#fff";
+    ctx.arc(screenWidth / 2 - 64, screenHeight / 2 + 65, 25, 0 + 0.5 * Math.PI, 1.5 * Math.PI);
+    ctx.fill();
+
+    ctx.save();
+
+    //画一个三角形
+    ctx.beginPath();
+    var startx = screenWidth / 2 - 62;
+    var starty = screenHeight / 2 + 50;
+    ctx.moveTo(startx, starty);
+    ctx.lineTo(startx + 23, starty + 15);
+    ctx.lineTo(startx, starty + 30);
+    ctx.lineTo(startx, starty);
+    ctx.fillStyle = "#588ddc";
+    ctx.fill();
+    ctx.save();
+
+    ctx.fillStyle = "#000";
+    ctx.font = "bold 23px Arial"
+    ctx.fillText(
+      "开始游戏",
+      screenWidth / 2 - 28,
+      screenHeight / 2 + 73
+    )
+
+    ctx.beginPath();
+    var width = canvas.width,
+      height = canvas.height;
+    if (window.devicePixelRatio) {
+
+    }
+
+    // //画一条线
+    ctx.beginPath();
+    ctx.moveTo(0, screenHeight - 150);
+    ctx.lineTo(screenWidth, screenHeight - 150);
+    ctx.strokeStyle = "#fff"
+    ctx.stroke();
+
+
+
+    ctx.beginPath();
+    ctx.arc(screenWidth / 2 - 100, screenHeight - 80, 40, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.font = "bold 23px Arial"
+    ctx.fillStyle = "#fff";
+    ctx.fillText(
+      "排行",
+      screenWidth / 2 - 123,
+      screenHeight - 74
+    )
+
+
+
+    ctx.beginPath();
+    ctx.arc(screenWidth / 2, screenHeight - 80, 40, 0, 2 * Math.PI);
+    ctx.stroke();
+
+    ctx.font = "bold 23px Arial"
+    ctx.fillStyle = "#fff";
+    ctx.fillText(
+      "分享",
+      screenWidth / 2 - 22,
+      screenHeight - 74
+    )
+    ctx.beginPath();
+    ctx.arc(screenWidth / 2 + 100, screenHeight - 80, 40, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.fillText(
+      "设置",
+      screenWidth / 2 + 77,
+      screenHeight - 74
+    )
   }
 }
